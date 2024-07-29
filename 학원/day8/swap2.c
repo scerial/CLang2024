c39ad57ac062df92a054a6be4b6a2156e1dae867
Jacob Kim DT <jacobkim0707@gmail.com>
2024-07-29 23:56:32 +0900

    swap properly done with pointers data in main could be manipulated from a function using pointers

--- 학원/day8/swap2.c
+++ 학원/day8/swap2.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
+void swap(int *a, int *b)
+{
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
 int main()
 {
     int i=10, j=20;
-    int *ptri=&i, *ptrj=&j;
-    
-    int temp=ptri;
-    ptri=ptrj;
-    ptrj=temp;
+    printf("i=%d, j=%d\n",i,j);
 
-    
+    swap(&i,&j);
+    printf("i=%d, j=%d",i,j);
 
+    return 0;
 }
